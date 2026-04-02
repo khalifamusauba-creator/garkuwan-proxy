b2b06d728d480b0f7d5a854be52730c5eb83f34d
khalifamusauba-creator <khalifamusauba@gmail.com>
2026-04-02 23:59:48 +0100

    Update ask.py

--- functions/ask.py
+++ functions/ask.py
@@ -4,7 +4,7 @@ import os
 
 def handler(event, context):
     # Wannan zai dauko key din daga Netlify Settings
-    api_key = os.environ.get("GEMINI_API_KEY")
+    api_key = api_key = "AIzaSyAi6IR674TJEerKVWdqVOdqFmHTADjWlsg"
     
     if not api_key:
         return {"statusCode": 500, "body": json.dumps({"error": "API Key is missing in Netlify settings"})}
